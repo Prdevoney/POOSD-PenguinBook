9d8861367d927e488e8731c0c84bef2c81818c46
John Trucillo <156385599+Shmoofy@users.noreply.github.com>
2024-01-17 03:00:22 -0500

    Add files via upload

---
+++ LAMPAPI/AddContact.php
@@ -0,0 +1,39 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$contact = $inData["contact"];
+	$userId = $inData["userId"];
+
+	$conn = new mysqli("localhost", "TheBeast", "POOSD-2024-Spring", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Contacts (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $contact);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
