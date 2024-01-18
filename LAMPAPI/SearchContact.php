f603c29f43c2602632487c544765d76345bbddd4
Adrian Hernandez <86635100+ahernandezgely@users.noreply.github.com>
2024-01-18 03:29:33 -0500

    Add files via upload

--- LAMPAPI/SearchContact.php
+++ LAMPAPI/SearchContact.php
@@ -1,68 +1,51 @@
 <?php
+$inData = getRequestInfo();
 
-	$inData = getRequestInfo();
-	
-	$searchResults = "";
-	$searchCount = 0;
+$name = $inData["name"];
+$userId = $inData["userId"];
 
-	$conn = new mysqli("localhost", "TheBeast", "POOSD-2024-Spring", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("select Name from Contacts where Name like ? and UserID=?");
-		$contactName = "%" . $inData["search"] . "%";
-		$stmt->bind_param("ss", $contactName, $inData["userId"]);
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
-		
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '"' . $row["Name"] . '"';
-		}
-		
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found" );
-		}
-		else
-		{
-			returnWithInfo( $searchResults );
-		}
-		
-		$stmt->close();
-		$conn->close();
-	}
+$conn = new mysqli("localhost", "TheBeast", "POOSD-2024-Spring", "COP4331");
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+if ($conn->connect_error) {
+    returnWithError($conn->connect_error);
+} else {
+    $stmt = $conn->prepare("SELECT * FROM Contacts WHERE UserID = ? AND BINARY Name LIKE ?");
+    $searchPattern = "%" . $name . "%";
+    $stmt->bind_param("is", $userId, $searchPattern);
+    $stmt->execute();
+    
+    $result = $stmt->get_result();
+    
+    $contacts = array();
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    while ($row = $result->fetch_assoc()) {
+        $contacts[] = $row;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+    if (empty($contacts)) {
+        sendResultInfoAsJson(json_encode(["message" => "No contacts found."]));
+    } else {
+        sendResultInfoAsJson(json_encode($contacts));
+    }
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
 ?>
