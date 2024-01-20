226969e1217da6f679e1d6047664bf2d929209a0
Adrian Hernandez <86635100+ahernandezgely@users.noreply.github.com>
2024-01-20 02:17:17 -0500

    Update AddContact.php
    PB-8

--- LAMPAPI/AddContact.php
+++ LAMPAPI/AddContact.php
@@ -1,7 +1,8 @@
 <?php
 	$inData = getRequestInfo();
-	
-	$name = $inData["name"];
+
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
 	$phone = $inData["phone"];
 	$email = $inData["email"];
 	$userId = $inData["userId"];
@@ -13,8 +14,8 @@
 	} 
 	else
 	{
-		$stmt = $conn->prepare("INSERT into Contacts (Name, Phone, Email, UserID) VALUES(?,?,?,?)");
-		$stmt->bind_param("sssi", $name, $phone, $email, $userId);
+		$stmt = $conn->prepare("INSERT into Contacts (FirstName, LastName, Phone, Email, UserID) VALUES(?,?,?,?,?)");
+		$stmt->bind_param("ssssi", $firstName, $lastName, $phone, $email, $userId);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
