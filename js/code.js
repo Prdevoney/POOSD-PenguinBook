87e2f74fd328f3232b09923a8192607703af3140
YutoKik <yutodkik0528@gmail.com>
2024-02-03 04:02:12 -0500

    Update code.js

--- js/code.js
+++ js/code.js
@@ -251,7 +251,7 @@ function searchContact() {
                 for (let i = 0; i < results.length; i++) {
                     text += "<tr id='row" + i + "'>"
                     text += "<td id='first_Name" + i + "'><span>" + results[i].FirstName + "</span></td>";
-                    text += "<td id='last_Name" + i + "'><span>" + results[i].LastName + "</span></td>";
+                    text += "<td id='last_Name" + i + "'><span>" + results[i].LastNam + "</span></td>";
                     text += "<td id='email" + i + "'><span>" + results[i].Email + "</span></td>";
                     text += "<td id='phone" + i + "'><span>" + results[i].Phone + "</span></td>";
                     text += "<tr/>"
